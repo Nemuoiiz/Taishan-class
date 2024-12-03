7d8d6be1e0cbfd34d6ff43b7152f50cb93f3d283
Diane Zhang <rain436112@gmail.com>
2024-12-03 16:12:43 +0800

    [feat] 新增：12/03 JS 上課內容（Vite 與單元件檔案）

---
+++ Html and JS/2. JAVA SCRIPT/2024-12-03/vue-project/src/components/LeftMenuButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="btn">{{ text }}</div>
+</template>
+
+<style scoped lang="sass">
+.btn
+  width: 200px
+  height: 35px
+  text-align: center
+  background: url(@/assets/menu1.png) no-repeat center
+  margin: 5px auto
+
+  // 內容是單行 => align-items
+  // 內容是多行 => align-content
+  align-content: center
+  &:hover
+    background: url(@/assets/menu1.png) no-repeat center
+</style>
+
+<script setup>
+defineProps(['text'])
+</script>
+
+<!--
+<script>
+export default {
+  props: ['text'],
+}
+</script>
+-->
---
+++ Html and JS/2. JAVA SCRIPT/2024-12-03/vue-project/src/components/TopImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="top">
+    <img src="@/assets/logo.png" alt="" />
+  </div>
+</template>
+
+<!--  沒有寫代表全域
+      scoped 代表樣式只會套用到這個 vue 檔案 -->
+<style scoped lang="sass">
+#top
+  width: 1000px
+  height: 65px
+  text-align: center
+  img
+    width: 994px
+    height: 60px
+</style>
