c8e0ded66b7371050e60f27bf5361f507d99cee4
Diane Zhang <rain436112@gmail.com>
2024-12-03 16:17:05 +0800

    [feat] 新增：12/03 JS 老師版本（Vite 與單元件檔案）

---
+++ Html and JS/2. JAVA SCRIPT/2024-12-03/0. Teacher/vue-project/src/components/LeftUpdate.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="update">最近更新日期：{{ date }}</div>
+</template>
+
+<script setup>
+const date = new Date().toLocaleDateString()
+</script>
+
+<style scoped lang="sass">
+#update
+  width: 215px
+  height: 25px
+  font-family: "標楷體"
+  font-size: 16px
+  color: #000
+  text-align: center
+</style>
---
+++ Html and JS/2. JAVA SCRIPT/2024-12-03/0. Teacher/vue-project/src/components/RightMarquee.vue
@@ -0,0 +1,12 @@
+<template>
+  <marquee>歡迎光臨校園社團介紹網，參觀後請支持社團活動並熱烈參與</marquee>
+</template>
+
+<style scoped lang="sass">
+marquee
+  width: 770px
+  height: 29px
+  font-family: "標楷體"
+  font-size: 24px
+  color: #00f
+</style>
