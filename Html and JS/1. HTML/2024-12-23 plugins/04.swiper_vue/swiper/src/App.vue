f44ff2ad33653c71233a8dfc90d752fc41eb80fd
Diane Zhang <rain436112@gmail.com>
2024-12-23 16:33:42 +0800

    [feat] 新增：12/23 HTML 上課內容

---
+++ Html and JS/1. HTML/2024-12-23 plugins/04.swiper_vue/swiper/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-conatiner>
+        <!-- <router-view /> -->
+        <swiper
+          :pagination="{
+            dynamicBullets: true,
+          }"
+          :navigation="true"
+          :modules="modules"
+          class="mySwiper"
+        >
+          <swiper-slide
+            v-for="(item,index) of imgArray"
+            :key="index"
+          >
+            <img :src="item">
+          </swiper-slide>
+        </swiper>
+      </v-conatiner>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+// 引入 swiper
+import { Swiper, SwiperSlide } from 'swiper/vue';
+// 引入功能模組
+import { Navigation, Pagination } from 'swiper/modules';
+// swiper 樣式
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const imgArray = reactive([
+  'https://picsum.photos/600/300/?random=1',
+  'https://picsum.photos/600/300/?random=2',
+  'https://picsum.photos/600/300/?random=3',
+  'https://picsum.photos/600/300/?random=4',
+  'https://picsum.photos/600/300/?random=5',
+  'https://picsum.photos/600/300/?random=6',
+  'https://picsum.photos/600/300/?random=7',
+  'https://picsum.photos/600/300/?random=8',
+])
+
+// swiper 模組
+const modules = [Navigation, Pagination]
+
+</script>
+
+<style>
+
+.mySwiper{
+  width: 600px;
+  height: 300px;
+}
+</style>
---
+++ Html and JS/1. HTML/2024-12-23 plugins/ReadMe.md
@@ -0,0 +1,9 @@
+套件：
+
+1. **particles**：https://vincentgarreau.com/particles.js/
+   （GITHUB：https://github.com/VincentGarreau/particles.js/）
+2. **aos**：https://michalsnik.github.io/aos/
+   （GITHUB：https://github.com/michalsnik/aos/tree/v2）
+3. **wow**：https://wowjs.uk/
+4. **swiper** ：https://swiperjs.com/
+   （GITHUB：https://github.com/nolimits4web/swiper）
