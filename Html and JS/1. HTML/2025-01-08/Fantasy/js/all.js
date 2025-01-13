09cafa003ff04ff817819234b6ba6d21de05f7ed
Diane Zhang <rain436112@gmail.com>
2025-01-13 16:24:47 +0800

    [feat] 新增：20250113 HTML 上課內容

--- Html and JS/1. HTML/2025-01-08/Fantasy/js/all.js
+++ Html and JS/1. HTML/2025-01-08/Fantasy/js/all.js
@@ -58,3 +58,215 @@ const swiper = new Swiper('#swiper', {
         prevEl: ".swiper-button-prev",
 	},
 })
+
+// GSAP ---------------------------------------------------------------------------------
+// 註冊 plugin
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+
+$('#navbar .main-link, .backtop a').each(function (index, link) {
+	$(this).on('click', function (e) {
+		e.preventDefault()	// 阻止 a 連結預設的動作
+		if ($(this).attr('herf') == '#section04' || $(this).attr('href') == '#section05') {
+			gsap.to(window, {
+				// 指定滑動
+				scrollTo: {
+					y:`#section0${index + 1}`
+				},
+				duration: 1.5,
+				ease:'back.inOut'
+			})
+		} else {
+			gsap.to(window, {
+				scrollTo: {
+					y: `#section0${index + 1}`,
+					offsetY: 150,
+				},
+				duration: 1.5,
+				ease:'back.inOut',
+			})
+		}
+	})
+})
+
+// 導覽列 active ---------------------------------------------------------------
+$('.main-link').each(function (index, link) {
+	let id = $(link).attr('href')		// jQ 的 getter
+	// console.log(link,id)
+	gsap.to(link, {
+		scrollTrigger: {
+			trigger: `${id}`,
+			start: 'top center',
+			end: 'bottom center',
+			toggleClass: {
+				targets: link,
+				className:'active',		// .active 有設定 css 黃色字
+			},
+			// markers: true,
+		}
+	})
+})
+
+// 導覽列收合 ---------------------------------------------------------------
+// const tween = gsap.from('#navbar', {
+gsap.from('#navbar', {		// 上列可省略為這段
+	yPercent: -100,
+	paused: false,
+	duration: 0.5,
+	scrollTrigger: {
+		start: 'top 60',
+		end: () => '+=' + document.documentElement.scrollHeight,		// 抓到整個文件的高度
+		onEnter(self) {
+			// console.log(self)
+			self.animation.play()	// 進入時播放
+		},
+		// onUpdate 是在 scrollTrigger 處於活動狀態時，每次滾動時都會觸發
+		onUpdate(self) {
+			console.log(self.direction)
+
+			// -1 往上正向播放，1 往下反向播放
+			// 三元運算值寫法
+			self.direction === -1 ? self.animation.play() : self.animation.reverse()
+			// // 一般寫法
+			// if (self.direction === -1) {
+			// 	self.animation.play()
+			// } else {
+			// 	self.animation.reverse()
+			// }
+		},
+		// markers: true,
+	}
+})
+
+
+// 霧的動畫
+$('.fog').each(function (index, fog) {
+	gsap.set(fog, {
+		width: '100%',
+		height: '100%',
+		background: `url('../images/fog.png') no-repeat center/80%`,
+		opacity: 0.8,
+		position: 'fixed',
+		top: 'random(0, 100)' + '%',
+
+		// x: function (a, b, c) {
+		x: function () {
+			// console.log(a,b,c)		// 取出數值後得到 index、自己本身元素、參與的陣列
+			// console.log(index)	
+
+			// 0,1,2,3
+			// 0,2(一組) => 畫面外的左邊
+			// 	 1,3	=> 畫面外的右邊
+			return index % 2 == 0 ? -$(window).width() : $(window).width()
+		}
+	})
+	gsap.to(fog, {
+		x: function () {
+			return index % 2 == 0 ? $(window).width() : -$(window).width()
+		},
+		// 當動畫重複播放時，將霧的位置隨機設定
+		onRepeat() {
+			$(fog).css({
+				top: gsap.utils.random(0,100)+ '%'
+			})
+		},
+		duration: 45,
+		repeat: -1,
+		ease:'none',
+	})
+})
+
+// 星空背景 --------------------------------------------------------------------
+// _global.scss
+gsap.to('body', {
+	scrollTrigger: {
+		trigger: 'body',
+		start: 'top 0%',
+		end: 'bottom 0%',
+		scrub: 5,
+		markers:true,
+	},
+	backgroundPosition: '50% 100%',
+	// scale: 0,
+	// rotation:3600,
+	// opacity:0,
+	ease: 'none',
+})
+
+
+// backtop ---------------------------------------------------------------------
+gsap.to('.backtop', {
+	scrollTrigger: {
+		trigger: '#footer',
+		start: 'top bottom',
+		end: '100% bottom',
+		// 往下時綠碰綠、往下時紅碰紅、往上時紅碰紅、往上時綠碰綠
+		toggleActions: 'play none none reverse' ,
+		// markers:true,
+	},
+	// 進入區域時出現
+	display: 'block',
+	opacity: 1,
+	duration: 1,
+})
+
+// 流星 ------------------------------------------------------------------------
+// 1. 創建流星的數目
+function createStar(starNumber) {
+	for (let i = 0; i < starNumber; i++){
+		$('.shooting_star').append('<div class="star"></div>')
+	}
+
+	// 拿到 nodeList
+	// const stars = document.querySelectorAll('.star')
+	// console.log(stars)
+
+	// 拿到一般陣列
+	// const stars = Array.from(document.querySelectorAll('.star'))
+	const stars = gsap.utils.toArray('.star')
+	// console.log(stars)
+	return stars
+}
+
+// 2. 設定補間動畫的預設值
+function setStarTween(stars) {
+	gsap.set('.shooting_star', {
+		perspective: 800			// 讓整個舞台有 3D 效果 (透視遠近的感覺)
+	})
+
+	stars.forEach(function (star, index) {
+		gsap.set(star, {
+			transformOrigin: '0 50%',		// 以左邊中央為基準點 (類似時鐘)
+			position: 'absolute',
+			// 假設 window 寬度 1000
+			// 1000/2 = 500
+			// 1000*2 = 2000
+			left: gsap.utils.random($(window).width() / 2, $(window).width() * 2),
+			top: gsap.utils.random(-100, -200),
+			rotation:-25,
+		})
+	})
+	return stars
+}
+
+// 3. 設定流星群的動畫
+function playStarTimeline(stars) {
+	const tl = gsap.timeline({
+		repeat: -1
+	})
+	tl.to(stars, {
+		x: `-=${$(window).width() * 1.5}`,		// 流星往左
+		y: `+=${$(window).height() * 1.5}`,		// 流星往右
+		z: `random(-100, 500)`,
+
+		stagger: function (index, target, targets) {
+			return gsap.utils.random(index + 5, (index + 5) * 2 , 1)	
+		},
+		duration: 'random(0.5, 3, 0.1)',		// 0.5、0.6、0.7......3 其中一個
+		ease: 'none',
+	})
+}
+
+// 管道設計模式，可以將一整個功能拆解成多個小功能
+// 會將函式功能串接在一起，第一個函式回傳值會傳給第二個函式，第二個函式回傳值會傳給第三個函式，以此類推……
+const playStar = gsap.utils.pipe(createStar, setStarTween, playStarTimeline)
+playStar(30)
