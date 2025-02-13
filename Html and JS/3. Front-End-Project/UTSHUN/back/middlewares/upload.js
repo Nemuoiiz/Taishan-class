2c060e1f4296a892fbda91b59380979542668205
Daine Zhang <rain436112@gmail.com>
2025-02-13 23:36:05 +0800

    [feat] 新增：project

---
+++ Html and JS/3. Front-End-Project/UTSHUN/back/middlewares/upload.js
@@ -0,0 +1,14 @@
+import multer from 'multer'
+import { v2 as cloudinary } from 'cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import { StatusCodes } from 'http-status-codes'
+
+// 設定 cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+// 設定上傳方
+const 
